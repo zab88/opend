d9c4534d8793763797df0202243b18e9cac13fd1
zab88 <alexey.zabaykin@gmail.com>
2017-12-09 22:31:52 +0700

    sber test ok

--- visualization/python/reader_01.py
+++ visualization/python/reader_01.py
@@ -9,3 +9,15 @@ dd = dd[dd['region'].str.contains("Новосибирск")]
 #     print(r['name'], r['region'], r['date'], r['value'])
 
 print(set(dd.name))
+
+for some_col in list(set(dd.name)):
+    some_set = dd[dd['name'].str.contains(some_col)]
+
+    # print(some_set)
+
+    for e, row in some_set.iterrows():
+        some_date = row['date'].split('-')
+        print("[new Date({}, {}, {}), {}],".format(some_date[0], some_date[1], some_date[2], row['value']))
+
+
+    exit()
